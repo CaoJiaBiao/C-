6b59004a328a4245138197a00095aa3b0cba3ab9
CaoJiaBiao <2467697276@qq.com>
2018-11-29 09:36:24 +0800

    第二天：默认参数和占位符

---
+++ C++基础02/2 默认参数和占位符/2 默认参数和占位符.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void printAandB(int a, int b=100)//默认实参要在尾部
+{
+	cout << "A=" << a << ",B=" << b << endl;
+}
+void printAandB(int a, int)//占位符，不给，可以是(int a, int=0)
+{
+	cout << "A=" << a << endl;
+}
+int main(void)
+{
+	int a = 10, b = 20;
+	printAandB(a);
+	return 0;
+}
