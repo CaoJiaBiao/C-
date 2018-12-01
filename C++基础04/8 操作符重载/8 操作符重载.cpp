d0f0cc927d2a2266d2b99aa8ae78eed131a1de08
CaoJiaBiao <2467697276@qq.com>
2018-12-01 19:07:51 +0800

    第四天完成，运算符重载完成

--- C++基础04/8 操作符重载/8 操作符重载.cpp
+++ C++基础04/8 操作符重载/8 操作符重载.cpp
@@ -30,6 +30,16 @@ public:
 
 		return *this;
 	}
+	//前缀
+	Complex& operator ++() {//加上&是为了能一直调用，如++++++a
+	}
+	/*
+	const Complex& operator ++() {//加上&是为了能一直调用，如++++++a
+	}表示++只能用一次，即不能a++++;
+	*/
+	//后缀
+	Complex& operator ++(int) {
+	}
 	//单目运算符
 	//调用：对象名.operator-();
 			// -对象名
@@ -37,15 +47,40 @@ public:
 		a = -a;
 		b = -b;
 	}
+	/*
+	//cout<<a;成员函数要写在cout里面，所以没有写在成员函数中
+	//如果写在Complex里面，只能写成，a<<cout;
+	ostream& operator <<( Complex &c) {
+		os << "a=" << c.a << "b=" << c.b << endl;
+		return os;
+	}
+
+	*/
 private:
 	int a; //实数部分
 	int b;//虚数部分
 };
 #if 0
+//全局和成员函数的区别只是有没有放第一个变量
 //全局提供一个+号操作符重载
+//这些要声明为complex的友元函数
 Complex operator+(Complex &c1, Complex &c2)
 {
 	Complex temp(c1.a + c2.a, c1.b + c2.b);
 	return temp;
 }
+Complex& operator++(Complex &c1, int)//占位操作符，用来表示后缀，亚元
+{
+	Complex temp(c1.a + c2.a, c1.b + c2.b);
+	return temp;
+}
+ostream& operator <<(ostream &os, Complex &c) {
+	os << "a=" << c.a << "b=" << c.b << endl;
+	return os;
+}//用法cout<<c2;
+istream& operator >>(istream &is, Complex &c) {
+	cout << "a" << endl;
+	is >> c.a;
+	return is;
+}//用法cout<<c2;
 #endif
